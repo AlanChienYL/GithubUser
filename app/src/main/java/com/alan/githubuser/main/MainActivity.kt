111f9e39fabe40109c120c57778746b7652b3bc8
Alan Jian <jianyulun@qiyi.com>
2020-12-14 16:48:53 +0800

    Add MainActivity

---
+++ app/src/main/java/com/alan/githubuser/main/MainActivity.kt
@@ -0,0 +1,9 @@
+package com.alan.githubuser.main
+
+import com.alan.githubuser.R
+import com.alan.githubuser.base.activity.BaseActivity
+import com.alan.githubuser.databinding.ActivityMainBinding
+
+class MainActivity : BaseActivity<ActivityMainBinding>() {
+    override fun getLayoutId(): Int = R.layout.activity_main
+}
