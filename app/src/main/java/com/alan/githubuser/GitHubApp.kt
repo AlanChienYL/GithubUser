726e92cc332d12dc17be78f81d68e08eb16e22f9
AlanChien <alan50637@gmail.com>
2020-12-20 23:30:34 +0800

    Enable StrictMode in debug mode

---
+++ app/src/main/java/com/alan/githubuser/GitHubApp.kt
@@ -0,0 +1,38 @@
+package com.alan.githubuser
+
+import android.app.Application
+import android.os.StrictMode
+import android.os.StrictMode.ThreadPolicy
+import android.os.StrictMode.VmPolicy
+
+/**
+ * Created by alan on 20,December,2020.
+ */
+class GitHubApp : Application() {
+
+    override fun onCreate() {
+        if (BuildConfig.DEBUG) {
+            enableStrictMode()
+        }
+        super.onCreate()
+    }
+
+    private fun enableStrictMode() {
+        StrictMode.setThreadPolicy(
+            ThreadPolicy.Builder()
+                .detectDiskReads()
+                .detectDiskWrites()
+                .detectNetwork()
+                .penaltyLog()
+                .build()
+        )
+        StrictMode.setVmPolicy(
+            VmPolicy.Builder()
+                .detectLeakedSqlLiteObjects()
+                .detectLeakedClosableObjects()
+                .penaltyLog()
+                .penaltyDeath()
+                .build()
+        )
+    }
+}
