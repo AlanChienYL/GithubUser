d058ad9dae4b973fe6fee64b7f2399ff8d383f73
AlanChien <alan50637@gmail.com>
2020-12-23 22:59:49 +0800

    Execute spotless apply

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,9 +1,11 @@
 object Versions {
+
     const val kotlin = "1.4.21"
     const val buildGradleTool = "4.1.1"
     const val spotless = "4.5.1"
 
-    object Android{
+    object Android {
+
         const val compileSdkVersion = 30
         const val minSdkVersion = 23
         const val targetSdkVersion = 30
@@ -13,7 +15,8 @@ object Versions {
         const val constraintLayout = "2.0.4"
     }
 
-    object Test{
+    object Test {
+
         const val junit = "1.1.2"
         const val espresso = "3.3.0"
     }
